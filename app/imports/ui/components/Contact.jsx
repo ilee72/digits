c7e27c022ff5c8691a007b54974f10168696ae54
ilee72 <46728285+ilee72@users.noreply.github.com>
2019-03-28 11:02:35 -1000

    delete-contacts1

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import { Card, Image, Feed } from 'semantic-ui-react';
+import { Card, Image, Feed, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import Note from '/imports/ui/components/Note';
 import AddNote from '/imports/ui/components/AddNote';
+import { Contacts } from '/imports/api/contact/contact';
+/** Need this for bert alert **/
+import { Bert } from 'meteor/themeteorchef:bert';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Contact extends React.Component {
+/**Add a constructor that invokes super(props), then binds THIS in the onClick method. **/
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+/** Implement onClick. Should remove call on Contacts.remove with the _id for the current item and then passes this.deleteCallback **/
+  onClick() {
+    Contacts.remove(this.props.contact._id, this.deleteCallback);
+  }
+
+  /** Implement deleteCallback which says success or failed on bert alert **/
+  deleteCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Delete has failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Delete has succeeded' });
+    }
+  }
+
   render() {
     return (
         <Card centered>
@@ -28,6 +50,9 @@ class Contact extends React.Component {
           </Card.Content>
           <Card.Content extra>
             <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
+          </Card.Content>
+          <Card.Content extra>
+            <Button onClick={this.onClick}>Delete</Button>
           </Card.Content>
         </Card>
     );
